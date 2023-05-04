80532b3efd833df57ecb93b0f8ff9388bd63bb07
Nacho <nacho.dominguez.dev@gmail.com>
2023-05-04 16:05:36 +0200

    configure swagger in application.kt

--- src/main/kotlin/com/nacho/Application.kt
+++ src/main/kotlin/com/nacho/Application.kt
@@ -1,17 +1,20 @@
 package com.nacho
 
 import io.micronaut.runtime.Micronaut.run
-import io.swagger.v3.oas.annotations.*
-import io.swagger.v3.oas.annotations.info.*
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
 
 @OpenAPIDefinition(
-    info = Info(
-            title = "shared-expenses",
-            version = "0.0"
-    )
+	info = Info(
+		title = "Shared Expenses API",
+		version = "1.0",
+		description = "An API for managing shared expenses among friends"
+	),
+	servers = [Server(url = "http://localhost:8085")]
 )
-object Api {
-}
+object Api {}
+
 fun main(args: Array<String>) {
 	run(*args)
 }
